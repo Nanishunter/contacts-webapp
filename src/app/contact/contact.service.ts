15034c0d786edaa39e1812bf387290285e70041d
1701542-Saimia <anttiyleinen@hotmail.com>
2018-05-16 13:55:08 +0300

    Cleaned commented code from service, made environment for url.

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -8,24 +8,11 @@ import {Observable} from 'rxjs';
 })
 export class ContactService {
 
-  // contacts: Contact [];
-
 
   constructor(private contactHttpService: ContactHttpService) {
 
-    /*
-     this.contacts = [];
-     this.contacts.push(new Contact(1, 'Antti', 'Karjalainen'));
-     this.contacts.push(new Contact(2, 'Arto', 'Lindgren'));
-     this.contacts.push(new Contact(3, 'Iso', 'Mies'));
-     */
+
   }
-
-  /*
-  getContacts(): Contact[] {
-    return this.contacts;
-
-  */
 
 
   getContacts(): Observable<Contact[]> {
