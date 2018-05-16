50a3be82c7f840c5c7c05894d572830233aeb506
1701542-Saimia <anttiyleinen@hotmail.com>
2018-05-16 14:27:37 +0300

    Added properties for contacts. Showing up and working properly.

--- src/app/contact/contact-list/contact.ts
+++ src/app/contact/contact-list/contact.ts
@@ -2,10 +2,20 @@ export class Contact {
   id: number;
   firstName: string;
   lastName: string;
+  streetAddress: string;
+  city: string;
+  postalCode: string;
+  phoneNumber: string;
+  emailAddress: string;
 
 
-  constructor(id?: number, firstName?: string, lastName?: string) {
+  constructor(id?: number, streetAddress?: string, city?: string, postalCode?: string, phoneNumber?: string, emailAddress?: string, firstName?: string, lastName?: string) {
     this.id = id;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
     this.firstName = firstName;
     this.lastName = lastName;
   }
